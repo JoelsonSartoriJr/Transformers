1d7ab24f375cf88d9b188f96d21d2c10a7230338
Joelson Sartori <joelsonsartori@gmail.com>
2023-05-28 15:59:07 -0300

    feat: encoder

---
+++ transformers/encoder.py
@@ -0,0 +1,43 @@
+import torch
+import torch.nn as nn
+from transformer_block import TransformerBlock
+
+class Encoder(nn.Module):
+    def __init__(
+        self, 
+        src_vocab_size:int,
+        embed_size:int,
+        num_layers:int,
+        heads:int,
+        device:torch.device,
+        forward_expansion:int,
+        dropout:float,
+        max_length:int
+    ) -> None:
+        super(Encoder, self).__init__()
+        self.embed_size = embed_size
+        self.device = device
+        self.word_embedding = nn.Embedding(src_vocab_size, embed_size)
+        self.position_embedding = nn.Embedding(max_length, embed_size)
+        
+        self.layers = nn.ModuleList([
+            TransformerBlock(
+                embed_size,
+                heads,
+                dropout=dropout,
+                forward_expansion=forward_expansion
+            )
+        ])
+        
+        self.dropout = nn.Dropout(dropout)
+    
+    def forward(self, x:list, mask)->list:
+        N, seq_length = x.shape
+        pos = torch.arange(0, seq_length).expand(N, seq_length).to(self.device)
+        
+        out = self.dropout(self.word_embedding(x) + self.position_embedding(pos))
+        
+        for layer in self.layers:
+            out = layer(out, out, out, mask)
+            
+        return out
