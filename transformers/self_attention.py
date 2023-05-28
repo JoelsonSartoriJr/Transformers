8c07c680bb93ca8e5fb96605cfff7eedc8fd22a7
Joelson Sartori <joelsonsartori@gmail.com>
2023-05-28 17:40:14 -0300

    fix: error dim

--- transformers/self_attention.py
+++ transformers/self_attention.py
@@ -2,47 +2,53 @@ import torch
 import torch.nn as nn
 
 class SelfAttention(nn.Module):
-    def __init__(self, embed_size:int, heads:int) -> None:
+    def __init__(self, embed_size, heads):
         super(SelfAttention, self).__init__()
         self.embed_size = embed_size
         self.heads = heads
-        self.heads_dim = embed_size // heads
-        
-        assert (self.heads_dim * heads == embed_size), "Embed size needs to be divisible by heads"
-        
-        self.values = nn.Linear(self.heads_dim, self.heads_dim, bias=False)
-        self.keys = nn.Linear(self.heads_dim, self.heads_dim, bias=False)
-        self.queries = nn.Linear(self.heads_dim, self.heads_dim, bias=False)
+        self.head_dim = embed_size // heads
+
+        assert (
+            self.head_dim * heads == embed_size
+        ), "Embedding size needs to be divisible by heads"
+
+        self.values = nn.Linear(embed_size, embed_size)
+        self.keys = nn.Linear(embed_size, embed_size)
+        self.queries = nn.Linear(embed_size, embed_size)
         self.fc_out = nn.Linear(embed_size, embed_size)
-        
-    def forward(self, values:list, keys:list, queries:list, mask)->list:
-        N = queries.shape[0]
-        value_len, key_len, query_len = values.shape[1], keys.shape[1], queries.shape[1]
-        
-        values = self.values(values)
-        keys = self.keys(keys)
-        queries = self.queries(queries)
-        
-        values = values.reshape(N, value_len, self.heads, self.heads_dim)
-        key_len = keys.reshape(N, key_len, self.heads, self.heads_dim)
-        queries = queries.reshape(N, query_len, self.heads, self.heads_dim)
-        
-        attn = torch.einsum("nqhd, nkhd -> nhqk", [queries, keys])
-        #queries shape: (N, query_len, heads, heads_dim)
-        #keys shape: (N, key_len, heads, heads_dim)
-        #att shape: (N, heads, query_len, key_len)
-        
-        if mask is None:
-            attn = attn.masked_fill(mask==0, float("-1e20"))
-        
-        attention = torch.softmax(attn/(self.embed_size**(1/2)), dim=3)
-        
-        out = torch.eisum("nhql, nlhd -> nqhd", [attention, values]).reshape(
-            N, query_len, self.heads*self.heads_dim
+
+    def forward(self, values, keys, query, mask):
+        N = query.shape[0]
+
+        value_len, key_len, query_len = values.shape[1], keys.shape[1], query.shape[1]
+
+        values = self.values(values)  # (N, value_len, embed_size)
+        keys = self.keys(keys)  # (N, key_len, embed_size)
+        queries = self.queries(query)  # (N, query_len, embed_size)
+
+        # Split the embedding into self.heads different pieces
+        values = values.reshape(N, value_len, self.heads, self.head_dim)
+        keys = keys.reshape(N, key_len, self.heads, self.head_dim)
+        queries = queries.reshape(N, query_len, self.heads, self.head_dim)
+
+        energy = torch.einsum("nqhd,nkhd->nhqk", [queries, keys])
+        # queries shape: (N, query_len, heads, heads_dim),
+        # keys shape: (N, key_len, heads, heads_dim)
+        # energy: (N, heads, query_len, key_len)
+
+        if mask is not None:
+            energy = energy.masked_fill(mask == 0, float("-1e20"))
+
+
+        attention = torch.softmax(energy / (self.embed_size ** (1 / 2)), dim=3)
+
+        out = torch.einsum("nhql,nlhd->nqhd", [attention, values]).reshape(
+            N, query_len, self.heads * self.head_dim
         )
-        #attention shape: (N, heads, query_len, key_len)
-        #values shape: (N, value_len, heads, heads_dim)
-        #out shape: (N, query_len, heads, heads_dim)
-        
+        # attention shape: (N, heads, query_len, key_len)
+        # values shape: (N, value_len, heads, heads_dim)
+        # out after matrix multiply: (N, query_len, heads, head_dim), then
+
         out = self.fc_out(out)
+
         return out
