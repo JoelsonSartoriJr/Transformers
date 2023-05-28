3386ad8f8376130dbe063cf888b3cd353d0a219e
Joelson Sartori <joelsonsartori@gmail.com>
2023-05-28 16:50:51 -0300

    feat: verify model

--- transformers/transformer.py
+++ transformers/transformer.py
@@ -36,6 +36,17 @@ class Transformer(nn.Module):
             max_length
         )
         
+        self.decoder = Decoder(
+            trg_vocab_size,
+            embed_size,
+            num_layers,
+            heads,
+            forward_expansion,
+            dropout,
+            device,
+            max_length
+        )
+        
     def make_src_mask(self, src:list)->list:
         src_mask = (src != self.src_pad_idx).unsqueeze(1).unsqueeze(2)
         # (N, 1, 1, src_len)
@@ -52,3 +63,19 @@ class Transformer(nn.Module):
         encode_out = self.encoder(src, src_mask)
         out = self.decoder(trg, encode_out, src_mask, trg_mask)
         return out
+    
+    
+if __name__ == "__main__":
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(device)
+
+    x = torch.tensor([[1, 5, 6, 4, 3, 9, 5, 2, 0], [1, 8, 7, 3, 4, 5, 6, 7, 2]]).to(device)
+    trg = torch.tensor([[1, 7, 4, 3, 5, 9, 2, 0], [1, 5, 6, 2, 4, 7, 6, 2]]).to(device)
+
+    src_pad_idx = 0
+    trg_pad_idx = 0
+    src_vocab_size = 10
+    trg_vocab_size = 10
+    model = Transformer(src_vocab_size, trg_vocab_size, src_pad_idx, trg_pad_idx, device=device).to(device)
+    out = model(x, trg[:, :-1])
+    print(out.shape)
