e3b4cf3bc40c0c1fa2e1a430a5ec2a8fcd0d18a3
Joelson Sartori <joelsonsartori@gmail.com>
2023-05-28 17:40:24 -0300

    fix: order code

--- transformers/transformer.py
+++ transformers/transformer.py
@@ -10,20 +10,16 @@ class Transformer(nn.Module):
         trg_vocab_size:int,
         src_pad_idx:int,
         trg_pad_idx:int,
-        embed_size:int=256,
+        embed_size:int=512,
         num_layers:int=6,
         forward_expansion:int=4,
         heads:int=8,
         dropout:float=0,
-        device:torch.device=torch.device('cpu'),
+        device:str="cpu",
         max_length:int=100
     ):
 
         super(Transformer, self).__init__()
-        
-        self.src_pad_idx = src_pad_idx
-        self.trg_pad_idx = trg_pad_idx
-        self.device = device
         
         self.encoder = Encoder(
             src_vocab_size,
@@ -47,6 +43,10 @@ class Transformer(nn.Module):
             max_length
         )
         
+        self.src_pad_idx = src_pad_idx
+        self.trg_pad_idx = trg_pad_idx
+        self.device = device
+        
     def make_src_mask(self, src:list)->list:
         src_mask = (src != self.src_pad_idx).unsqueeze(1).unsqueeze(2)
         # (N, 1, 1, src_len)
@@ -69,13 +69,17 @@ if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(device)
 
-    x = torch.tensor([[1, 5, 6, 4, 3, 9, 5, 2, 0], [1, 8, 7, 3, 4, 5, 6, 7, 2]]).to(device)
+    x = torch.tensor([[1, 5, 6, 4, 3, 9, 5, 2, 0], [1, 8, 7, 3, 4, 5, 6, 7, 2]]).to(
+        device
+    )
     trg = torch.tensor([[1, 7, 4, 3, 5, 9, 2, 0], [1, 5, 6, 2, 4, 7, 6, 2]]).to(device)
 
     src_pad_idx = 0
     trg_pad_idx = 0
     src_vocab_size = 10
     trg_vocab_size = 10
-    model = Transformer(src_vocab_size, trg_vocab_size, src_pad_idx, trg_pad_idx, device=device).to(device)
+    model = Transformer(src_vocab_size, trg_vocab_size, src_pad_idx, trg_pad_idx, device=device).to(
+        device
+    )
     out = model(x, trg[:, :-1])
     print(out.shape)
