ce6d65683dfea9266eadfcb3671772399b42e34a
Joelson Sartori <joelsonsartori@gmail.com>
2023-05-30 13:51:27 -0300

    feat: train and evalueta model in one epoch

---
+++ utils/evaluate_one_epoch.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+
+def evaluate_one_epoch(model:torch.device, val_loader:DataLoader, criterion:nn, device: torch.device)->float:
+
+    model.eval()
+
+    epoch_loss = 0
+
+    with torch.no_grad():
+
+        for i, batch in enumerate(val_loader):
+            src, trg = batch
+            src, trg = src.permute(1,0).to(device), trg.permute(1,0).to(device)
+            output, _ = model(src, trg[:,:-1])
+
+            output_dim = output.shape[-1]
+
+            output = output.contiguous().view(-1, output_dim)
+            trg = trg[:,1:].contiguous().view(-1)
+
+            loss = criterion(output, trg)
+
+            epoch_loss += loss.item()
+
+    return epoch_loss / len(val_loader)
---
+++ utils/train_one_epoch.py
@@ -0,0 +1,35 @@
+import torch.nn as nn
+import torch
+from torch.utils.data import DataLoader
+from torch import optim
+
+def train_one_epoch(model:nn.Model, train_loader:DataLoader, optimizer:optim, criterion:nn, clip:int, device: torch.device)->float:
+
+    model.train()
+    
+    epoch_loss = 0
+    
+    for i, batch in enumerate(train_loader):
+        src, trg = batch
+        src, trg = src.permute(1,0).to(device), trg.permute(1,0).to(device)
+
+        optimizer.zero_grad()
+
+        output, _ = model(src, trg[:,:-1])
+
+        output_dim = output.shape[-1]
+
+        output = output.contiguous().view(-1, output_dim)
+        trg = trg[:,1:].contiguous().view(-1)
+
+        loss = criterion(output, trg)
+        
+        loss.backward()
+        
+        torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
+        
+        optimizer.step()
+        
+        epoch_loss += loss.item()
+        
+    return epoch_loss / len(train_loader)
