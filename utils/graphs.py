38db80fc9cfc2e6a784ef541dded8cd8e50de205
Joelson Sartori <joelsonsartori@gmail.com>
2023-05-31 19:33:12 -0300

    feat: plot attention

--- utils/graphs.py
+++ utils/graphs.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 
 def error_plot(history:dict)->None:
 
@@ -11,3 +12,27 @@ def error_plot(history:dict)->None:
     plt.legend()
 
     plt.show()
+    
+def display_attention(sentence:list, translation:list, attention:list, n_heads:int = 8, n_rows:int = 4, n_cols:int = 2):
+    
+    assert n_rows * n_cols == n_heads
+    
+    fig = plt.figure(figsize=(15,25))
+    
+    for i in range(n_heads):
+        
+        ax = fig.add_subplot(n_rows, n_cols, i+1)
+        
+        _attention = attention.squeeze(0)[i].cpu().detach().numpy()
+
+        cax = ax.matshow(_attention, cmap='bone')
+
+        ax.tick_params(labelsize=12)
+        ax.set_xticklabels(['']+[t.lower() for t in sentence], rotation=45)
+        ax.set_yticklabels(['']+translation)
+
+        ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+        ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+
+    plt.show()
+    plt.close()
