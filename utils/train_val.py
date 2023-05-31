7e95dba5fa6b83657e710c42a8c6d38bdc29cdbf
Joelson Sartori <joelsonsartori@gmail.com>
2023-05-31 18:42:24 -0300

    feat: save loss

--- utils/train_val.py
+++ utils/train_val.py
@@ -10,7 +10,11 @@ import math
 
 def train_val(epochs:int, train_loader:DataLoader, val_loader:DataLoader, model, optimizer:optim, criterion, clip:int, device: torch.device)->list:
     best_valid_loss = float('inf')
-
+    result = {
+        'epoch': [],
+        'train_loss': [],
+        'val_loss': [],
+    }
     for epoch in range(epochs):
 
         start_time = time()
@@ -21,7 +25,11 @@ def train_val(epochs:int, train_loader:DataLoader, val_loader:DataLoader, model,
         end_time = time()
 
         epoch_mins, epoch_secs = epoch_time(start_time, end_time)
-
+        
+        result['epoch'].append(epoch+1)
+        result['train_loss'].append(train_loss)
+        result['val_loss'].append(valid_loss)
+        
         if valid_loss < best_valid_loss:
             best_valid_loss = valid_loss
             torch.save(model.state_dict(), 'tut6-model.pt')
@@ -30,4 +38,4 @@ def train_val(epochs:int, train_loader:DataLoader, val_loader:DataLoader, model,
         print(f'\tTrain Loss: {train_loss:.3f} | Train PPL: {math.exp(train_loss):7.3f}')
         print(f'\t Val. Loss: {valid_loss:.3f} |  Val. PPL: {math.exp(valid_loss):7.3f}')
     
-    return best_valid_loss, train_loss, valid_loss
+    return best_valid_loss, result
