f3cce7858509d5655c9247a0a4a9304a88d79b8a
Joelson Sartori <joelsonsartori@gmail.com>
2023-05-31 23:21:00 -0300

    feat: calculate bleu

--- utils/utils.py
+++ utils/utils.py
@@ -3,6 +3,9 @@ import numpy as np
 import torch
 import torch.nn as nn
 from time import time
+from torchtext.data.metrics import bleu_score
+from translate import translate_sentence
+
 
 def set_seed(seed):
     random.seed(seed)
@@ -22,3 +25,20 @@ def count_parameters(model)->int:
 def initialize_weights(m):
     if hasattr(m, 'weight') and m.weight.dim() > 1:
         nn.init.xavier_uniform_(m.weight.data)
+
+def calculate_bleu(data:list, src_vocab:list, trg_vocab:list, model, device:torch.device , max_len:int = 50)->float:
+    
+    trgs = []
+    pred_trgs = []
+    
+    for datum in list(data):
+        src, length, trg = datum        
+        pred_trg, _ = translate_sentence(src, src_vocab, trg_vocab, model, device, max_len)
+        
+        #cut off <eos> token
+        pred_trg = pred_trg[:-1]
+        trg = trg[1:-1]
+        pred_trgs.append(pred_trg)
+        trgs.append([trg])
+        
+    return bleu_score(pred_trgs, trgs)
